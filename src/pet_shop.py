f336ae7be9f730ae727e7a08fe556cb7a0b70306
CarlyCoding <carly_off@outlook.com>
2022-06-24 19:38:56 +0100

    Task 3 completed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,20 +3,35 @@
 # Error. 1.
 # error message requires get_pet_shop_name to be defined. 
 # def get_pet_shop_name using dictionary key. 
-def get_pet_shop_name(shop_name):
-    return shop_name["name"]
+from this import s
+
+
+def get_pet_shop_name(shop):
+    return shop["name"]
 
 # Error. 2.
 # error message requires get_total_cash to be defined.
 # def using the two dictionary keys admin & total cash. 
 
-def get_total_cash(cash_takings):
-    return cash_takings["admin"]["total_cash"]
+def get_total_cash(shop):
+    return shop["admin"]["total_cash"]
 
 # Error. 3. 
 # add_or_remove_cash requires definition. 
-# The function prior will return the total cash, 1000, which we need to add 10 to. 
-# 
+# Def wants 10 added to admin>total cash eg. cash 
+# From failed tests I ascertained it needs 2 arguments & parameters.
+# Add 10 to the get_total_cash function- access shop as before. 
+
+def add_or_remove_cash(shop, money):
+    shop["admin"]["total_cash"] += money
+
+# Error 4
+# Same as prior but with removal so same function but with -= 
+
+
+
+
+    
 
 
 
