4d6feda8e20f0a47ae308e2e3e4f39dddab08cab
CarlyCoding <carly_off@outlook.com>
2022-06-24 18:01:44 +0100

    Task two completed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,4 +1,18 @@
+
+
+# Error. 1.
 # error message requires get_pet_shop_name to be defined. 
 # def get_pet_shop_name using dictionary key. 
 def get_pet_shop_name(shop_name):
     return shop_name["name"]
+
+# Error. 2.
+# error message requires get_total_cash to be defined.
+# def using the two dictionary keys admin & total cash. 
+
+def get_total_cash(cash_takings):
+    return cash_takings["admin"]["total_cash"]
+
+
+
+
