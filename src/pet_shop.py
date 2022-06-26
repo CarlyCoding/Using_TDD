c70ffff3ed5605561b3bea4604a0d8254f7c75af
CarlyCoding <carly_off@outlook.com>
2022-06-26 17:33:55 +0100

    Error 16 completed.

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -118,8 +118,12 @@ def remove_customer_cash(customer, money):
 def get_customer_pet_count(customer):
     return len(customer["pets"])
 
+# Error 16 
+# add_pet_to_customer requires definition.
+# Append Bors the Yonger (new_pet) to Alices pets (customer [0]) using append dict key. 
 
-
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
 
 
 
